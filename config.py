318a03ef21a416f14619cb456b4037d41f0ffc3d
Wojtini <56893313+Wojtini@users.noreply.github.com>
2021-08-03 13:07:36 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,28 @@
+#database connection
+DB_USER = 'root'
+DB_PASSWORD = 'root'
+DB_DATABASE = 'esport'
+DB_HOST = '127.0.0.1'
+
+
+#riot api 
+RIOT_API_KEY = XXX
+
+#league of legends client info
+LOL_CLIENT_LOCKFILE = 'D:\Riot Games\League of Legends\lockfile'
+# verify_file = 'riotgames.pem'
+
+#discord api
+DISCORD_TOKEN = XXX
+
+#esportmode
+ESPORT_GUILD = XXX
+ESPORT_TEXT_CHANNEL = XXX
+ESPORT_VOICE_CHANNEL = XXX
+
+#noob watcher
+NW_GUILD = XXX
+NW_TEXT_CHANNEL = XXX
+
+#ffmped executable
+FFMPEG_EXE = XXX
